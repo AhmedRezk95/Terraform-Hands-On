eca496cbe95809a6039eec609517f0380c46baf2
rizk <arezk95@gmail.com>
2022-07-16 19:26:05 +0200

    modify elastic cache resource

--- ElastiCache.tf
+++ ElastiCache.tf
@@ -3,12 +3,6 @@ resource "aws_elasticache_subnet_group" "cache_subnets" {
   subnet_ids = [module.network.subnet_id_pr1, module.network.subnet_id_pr2]
 }
 
-resource "aws_elasticache_security_group" "cache_sg" {
-  name                 = "elasticache-security-group"
-  security_group_names = [aws_security_group.allow-ssh.name, aws_security_group.allow-3000.name, aws_security_group.allow-3306.name, aws_security_group.allow-6379.name]
-
-}
-
 resource "aws_elasticache_cluster" "example" {
   cluster_id           = "cluster-example"
   engine               = "redis"
@@ -17,6 +11,7 @@ resource "aws_elasticache_cluster" "example" {
   parameter_group_name = "default.redis3.2"
   engine_version       = "3.2.10"
   port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.cache_subnets.name
   security_group_ids   = [aws_security_group.allow-ssh.id, aws_security_group.allow-3000.id, aws_security_group.allow-3306.id, aws_security_group.allow-6379.id]
 
 }
