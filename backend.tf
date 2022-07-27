cf108edc3f9d8d1eef98580adddd9f112c2a5d2d
rizk <arezk95@gmail.com>
2022-07-27 15:34:19 +0200

    change s3 bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "rizk-statefile"
+    bucket         = "rizk-statefile1"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "test"
