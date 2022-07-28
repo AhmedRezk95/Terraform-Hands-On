1fa2ee02418091cbd99d0df3190a69a4701abc60
rizk <arezk95@gmail.com>
2022-07-28 15:19:01 +0200

    add connection files to jenkins slave +  ansible-playbook

---
+++ connection-jenkins.tf
@@ -0,0 +1,42 @@
+resource "local_file" "pk" {
+    # filename -> place where it will be set
+    filename = "./aws-key.pem"
+    file_permission = 0400
+    # reference https://registry.terraform.io/providers/hashicorp/tls/latest/docs/resources/private_key
+    content = <<EOF
+    ${tls_private_key.myprivatekey.private_key_pem}
+    EOF
+}
+
+resource "local_file" "config-file" {
+    # create this resource after private key is create
+    depends_on = [
+      local_file.pk
+    ]
+    # filename -> place where it will be set
+    filename = "/root/.ssh/config"
+
+    # this content should make ssh jump-host we will need it when we setup something in private vm 
+    content = <<EOF
+    Host bastion-vm
+    HostName ${aws_instance.bastion-server.public_ip}
+    User ubuntu
+    IdentifyFile "./aws-key.pem"
+
+    Host ${aws_instance.application-server.private_ip}
+    User ubuntu
+    Port 22
+    ProxyCommand ssh -o StrictHostKeyChecking=no -A -W %h:%p -q bastion-vm
+    StrictHostKeyChecking no
+    IdentifyFile "./aws-key.pem"
+    EOF
+}
+
+resource "local_file" "ansible-inventory" {
+    # filename -> place where it will be set
+    filename = "./hosts"
+    content = <<EOF
+    [slave-host]
+    ${aws_instance.application-server.private_ip}
+    EOF
+}
